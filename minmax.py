ccda7e90b324b607269a12c45fcdb18e6d736057
adrenospy <amanmoon099@gmail.com>
2023-08-14 01:19:38 +0530

    completed the min-max algo

--- minmax.py
+++ minmax.py
@@ -1,106 +1,87 @@
 from tic_tac_toe import tic_tac_toe as t
 import numpy as np
+import random
 class node:
-    def __init__(self,state,player,parent=None):
+    def __init__(self,state,player,depth=0,parent=None):
         self.value=0
+        
         self.child=list()
         self.player=player 
         self.state=state
         self.parent=parent
-        self.win=0
+        self.depth=depth
         
-    def expand(self,computer_player):
-        if node.check_terminal(self,computer_player):
-            exit
+    def expand(self):
+        if self.check_terminal():      
+            return
         valid_moves=t.valid_moves(self.state)
         children=list()
         for move in valid_moves:
             child_state=t.state_modify(self.state.copy(),move,self.player)
-            children.append(node(child_state,-(self.player),self))
+            children.append(node(child_state,-(self.player),self.depth+1,self))
         self.child=children
         
-    def check_terminal(self,computer_player):
-        
+    def check_terminal(self):
         for row in range(3):
-            if np.sum(self.state[row, :] == 3*computer_player):
-                self.value=computer_player
-                self.win=1
+            if np.all(self.state[row, :] == -(self.player)):
+                self.value = -self.player/self.depth
                 return True
-                
+                    
         for col in range(3):
-            if np.sum(self.state[:, col] == 3*computer_player):
-                self.value=computer_player
-                self.win=1
+            if np.all(self.state[:, col] == -(self.player)):
+                self.value = -self.player/self.depth
                 return True
-
-        if np.trace(self.state) == computer_player * 3 or np.trace(np.fliplr(self.state)) == computer_player * 3:
-            self.value=computer_player
-            self.win=1
+    
+        if np.trace(self.state) == -(self.player * 3) or np.trace(np.fliplr(self.state)) == -(self.player * 3):
+            self.value = -self.player/self.depth
             return True
-
+    
         if 0 not in self.state.reshape(-1):
             return True
-        
-        for row in range(3):
-            if np.sum(self.state[row, :] == -3*computer_player):
-                self.value=-computer_player
-                return True
-                
-        for col in range(3):
-            if np.sum(self.state[:, col] == -3*computer_player):
-                self.value=-computer_player
-                return True
-
-        if np.trace(self.state) == self.player * -3 or np.trace(np.fliplr(self.state)) == computer_player * -3:
-            self.value=-computer_player
-            return True
-        
         return False
     
     def __repr__(self):
-        return f"{self.state}"
-        
-
-    def calculate_win(self,computer_player):
-        for children in self.child:
-            if children.value==self.player:
-                self.value=self.player
-            if children.value==-self.player:
-                self.value=-self.player
-            if children.value==computer_player:
-                self.win+=1
-                
-    def sigma_move(self,game_state,computer_player):
-        for children in self.child:
-            if children.value==-computer_player:
-                self.child.remove(children)
+        return f"{self.state}"          
             
-        bestmove_list=self.child.sort(key=lambda node:(node.win),reverse=True)
-        bestmove=bestmove_list.pop(0)
-        game_state=bestmove
-        return game_state
-    
-    @staticmethod
-    def node_search(node,computer_player):
-        topo=[]    
-        visited=set()
-        def build_topo(v):
-            if v not in visited:
-                v.expand()
-                visited.add(v)
-                for child in v.child:
-                    build_topo(child)
-                topo.append(v)
-        build_topo(node)
-        return topo
-        for nodes in topo:
-            if nodes.check_terminal(computer_player):
-                continue
-            nodes.calculate_win(computer_player)
+    def sigma_move_max(self):
+        maxvalue=list()
+        for children in self.child:
+            maxvalue.append(children.value)
+        if all(i == 0 for i in maxvalue):
+            return self.child[random.randrange(len(maxvalue))]
+        print(maxvalue)
+        index_max=maxvalue.index(max(maxvalue))
+        print(index_max)
+        return self.child[index_max]
+
+    def sigma_move_min(self):
+        minvalue=list()
+        for children in self.child:
+            minvalue.append(children.value)
+        if all(i == 0 for i in minvalue):
+            return self.child[random.randrange(len(minvalue))]
+        print(minvalue)
+        index_min=minvalue.index(min(minvalue))
+        print(index_min)
+        return self.child[index_min]
+        
+    def noob_move(self,action):
+        pass
+    def calculate_win(self):
+        child_value=list()
+        for children in self.child:
+            child_value.append(children.value)
+            
+        if self.player==1:
+            self.value=max(child_value)            
+        if self.player==-1:
+            self.value=min(child_value)
+        
     def get_layers_deepest_first(root,computer_player):
         current_layer=[root]
         next_layer=[]
         deepest_first_list=list()
+        
         while True:
             for childrens in current_layer:
                 childrens.expand(computer_player)
@@ -109,24 +90,24 @@ class node:
             current_layer=next_layer.copy()
             next_layer.clear()
             if current_layer==[]:
-                break    
+                break
         return deepest_first_list
+        
     def run_backprop(root,computer_player):
         list=node.get_layers_deepest_first(root,computer_player)
         for nodes in list:
-            if nodes.check_terminal(computer_player):
+            if nodes.check_terminal():
                 continue
-            nodes.calculate_win(computer_player)
-            
-def visualize_tree(node, depth=0):
-    indentation = "  " * depth
-    print(f"{indentation}Value: {node.value}, Win: {node.win}")
-    
-    for child in node.child:
-        visualize_tree(child, depth + 1)
-        
-game=t()
-state=game.initialise_state()
-sta=node(state,1)
+            nodes.calculate_win()
 
-print(node.run_backprop(sta,-1))
+
+
+
+
+
+# game=t()
+# state=game.initialise_state()
+# status=node(state,1)
+# node.run_backprop(status,-1)
+# status=status.sigma_move_max()
+# status=status.sigma_move_min()
---
+++ test.py
@@ -0,0 +1,5 @@
+marks = [12,31,40,40,0,32]
+
+ 
+# Using index() method
+print("Maximum Index position: ",marks.index(max(marks))) 
