11dab4469ae5f3734475b08d02206126ce6327a2
adrenospy <amanmoon099@gmail.com>
2023-08-14 01:19:38 +0530

    completed the min-max algo

--- tempCodeRunnerFile.py
+++
@@ -1,9 +0,0 @@
-visited.add(v)
-                for child in v.child:
-                    build_topo(child)
-                topo.append(v)
-        build_topo(initial_node)
-        for nodes in topo:
-            if nodes.check_terminal(computer_player):
-                continue            
-            nodes.calculate_win(computer_player)
